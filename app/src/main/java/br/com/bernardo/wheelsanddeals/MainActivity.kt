23c1eda60a5090a13bfd14647e2bd8200cdcf7e8
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-09-26 23:58:20 -0300

    First settings and appbar created

--- app/src/main/java/br/com/bernardo/wheelsanddeals/MainActivity.kt
+++ app/src/main/java/br/com/bernardo/wheelsanddeals/MainActivity.kt
@@ -1,10 +1,19 @@
 package br.com.bernardo.wheelsanddeals
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
+import br.com.bernardo.wheelsanddeals.databinding.ActivityMainLayoutBinding
 
 class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainLayoutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.statusBarColor = Color.parseColor("#040F0F")
     }
 }
